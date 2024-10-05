8f124e95bbd4d1aceceda4ebb36ee88cd98890a9
ABDUL HASEEB QAZI <qaziabdulhaseeb82@gmail.com>
2024-10-05 19:53:55 +0500

    portfolio
    port

---
+++ script/style-switcher.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+    styleSwitcherToggle.addEventListener("click", () => {
+        document.querySelector(".style-switcher").classList.toggle("open");
+        console.log("Toggler clicked!"); // Debugging log
+    });
+
+    window.addEventListener("scroll", () => {
+        if (document.querySelector(".style-switcher").classList.contains("open")) {
+            document.querySelector(".style-switcher").classList.remove("open");
+            console.log("Switcher closed on scroll!"); // Debugging log
+        }
+    });
+});
+
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+    if(color=== style.getAttribute("title"))
+    {
+    style.removeAttribute("disabled");
+    }
+    else
+    {
+    style.setAttribute("disabled", "true");
+    }
+})
+}
+
+
+
+
+
+
+// ---------------- theme light and dark -------------------
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun"); 
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
